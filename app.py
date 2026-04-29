8f7485f9a7d81550e1f5087477b7e900aec5b74b
Maram Temime <maramtemime@gmail.com>
2026-04-29 01:20:30 +0100

    Ajout d'une nouvelle route pour le chatbot

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template  
+from flask import Flask, render_template, request, jsonify  
 
 app = Flask(__name__)  
 
 @app.route("/")  
 def home():  
     return render_template("index.html")  
+
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    user_msg = request.json["message"]
+    return jsonify({"reply": "test: " + user_msg})
+
 
 if __name__ == "__main__":  
     app.run(debug=True)
