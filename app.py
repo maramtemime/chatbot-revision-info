d871523c08818c510fa529898e909b3f622fa61b
Ranim Soltani <ranimsoltani1234@gmail.com>
2026-05-03 19:35:56 +0200

    ajout du botton chat

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, jsonify
+import os
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+
+app = Flask(__name__)
+
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+@app.route("/cour")
+def cour():
+    return render_template("cour.html")
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    try:
+        user_msg = request.json["message"]
+
+        response = client.chat.completions.create(
+            model="llama-3.3-70b-versatile",
+            messages=[
+                {"role": "system", "content": "Tu es un assistant spécialisé en informatique. Réponds toujours en français."},
+                {"role": "user", "content": user_msg}
+            ]
+        )
+
+        reply = response.choices[0].message.content
+
+        return jsonify({"reply": reply})
+
+    except Exception as e:
+        return jsonify({"reply": "Error: " + str(e)})
+
+if __name__ == "__main__":
+    app.run(debug=True)
